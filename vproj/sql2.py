a49d546dbb88fedbf84584c0b634930e5e5f1394
Swathi N Rao <swat.snr98@gmail.com>
2018-08-05 16:51:21 +0530

    I have tabulated all the details

--- vproj/sql2.py
+++ vproj/sql2.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sql1 import Grocery,Base
+from tabulate import tabulate
 
 engine  = create_engine("sqlite:///grocery.db")
 
@@ -20,13 +21,34 @@ def add_products():
 	session.commit()
 
 def del_products():
-	product = input("enter product name")
-	price = int(input("enter the price"))
-	quantity = int(input("enter the quantity"))
-	delp = Grocery(product = product,price = price,quantity = quantity)
-	session.delete(delp)
+
+	list=[]
+	items=session.query(Grocery).all()
+	
+	list.append(("Id ","Product"))
+	#print(tabulate(v,tablefmt="fancy_grid"))
+	for l in items:
+		list.append((l.id,l.product))
+
+	print(tabulate(list,tablefmt="fancy_grid"))
+
+		
+
+
+
+	
+	i=int(input("Enter the id"))
+	session.query(Grocery).filter_by(id=i).delete()
 	session.commit()
 
+
 def display():
-	a = session.query(Grocery).all()	
-	return a
+	list=[]
+	items=session.query(Grocery).all()
+	
+	list.append(("Id ","Product","Price","Quantity"))
+	#print(tabulate(v,tablefmt="fancy_grid"))
+	for l in items:
+		list.append((l.id,l.product,l.price,l.quantity))
+
+	print(tabulate(list,tablefmt="fancy_grid"))
